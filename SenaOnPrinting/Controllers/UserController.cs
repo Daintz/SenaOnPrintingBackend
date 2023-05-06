bd8b13e3c6b13469d4d4245b5f085f8229999b3a
D4intzz <81586667+Daintz@users.noreply.github.com>
2023-05-06 15:38:31 -0500

    refactor: :recycle: accommodate structure
    accommodate structure in this project

---
+++ SenaOnPrinting/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using BusinessCape;
+using DataCape;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllersCape
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _service;
+
+        public UserController(UserService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<UserItem> GetAll()
+        {
+            return _service.GetUsers();
+        }
+
+        //// GET api/<UserController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<UserController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<UserController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<UserController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
