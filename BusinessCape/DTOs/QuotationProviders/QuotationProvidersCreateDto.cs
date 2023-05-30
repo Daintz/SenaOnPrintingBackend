c78e9ec47b00e6ab6a2faa8c1392d530b04a7842
CristianAlejandroQuintero <caquinterovelasquez@gmail.com>
2023-05-30 16:02:42 -0500

    feat: :sparkles: update api quotation providers
    update change status

--- BusinessCape/DTOs/QuotationProviders/QuotationProvidersCreateDto.cs
+++ BusinessCape/DTOs/QuotationProviders/QuotationProvidersCreateDto.cs
@@ -8,11 +8,13 @@ namespace BusinessCape.DTOs.QuotationProviders
 {
     public class QuotationProvidersCreateDto
     {
-        public long IdQuotationProvider { get; set; }
-        public DateTime? Date { get; set; }
+        public long Id { get; set; }
+        public DateTime? QuotationDate { get; set; }
         public byte[]? QuotationFile { get; set; }
         public double? FullValue { get; set; }
-        public long? IdProvider { get; set; }
+        public long? ProviderId { get; set; }
         public bool? StatedAt { get; set; }
     }
 }
+
+
