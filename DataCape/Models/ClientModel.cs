01e2e53d1e909496b78be9292c937c0e20fdc450
Kathecam <kathe.cadavid18@gmail.com>
2023-08-14 14:50:57 -0500

    feat: :sparkles: Add ClientModel
    Add ClientModel

---
+++ DataCape/Models/ClientModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCape.Models
+{
+    public class ClientModel
+    {
+        public ClientModel()
+        {
+            QuotationClients = new HashSet<QuotationClientModel>();
+        }
+
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Phone { get; set; }
+        public string Email { get; set; } = null!;
+        public string Center { get; set; } = null!;
+        public string Area { get; set; } = null!;
+        public string Regional { get; set; } = null!;
+        
+        public bool? StatedAt { get; set; }
+
+        //Relacion de uno a muchos con cotización
+        public virtual ICollection<QuotationClientModel> QuotationClients { get; set; }
+    }
+}
