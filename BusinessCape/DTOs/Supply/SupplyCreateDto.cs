61c7d84a408fa0a0bdd547f3aae94af5dc2f46c3
Fuanka <juanmanuelrestrepogarcia13@gmail.com>
2023-08-15 07:45:20 -0500

    build: fix bug supply
    fix bug supply

--- BusinessCape/DTOs/Supply/SupplyCreateDto.cs
+++ BusinessCape/DTOs/Supply/SupplyCreateDto.cs
@@ -4,7 +4,7 @@
     {
         public long IdSupply { get; set; }
         public string Name { get; set; } = null!;
-        public long IdUnitMeasure { get; set; }
+        //public long IdUnitMeasure { get; set; }
         public string? DangerIndicators { get; set; }
         public string? UseInstructions { get; set; }
         public string? Advices { get; set; }
@@ -12,7 +12,7 @@
         public int SortingWord { get; set; }
         public int Quantity { get; set; }
         public decimal? AverageCost { get; set; }
-        public long IdWarehouse { get; set; }
+        //public long IdWarehouse { get; set; }
         public bool? StatedAt { get; set; }
     }
 }
